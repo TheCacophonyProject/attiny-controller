9c800c599c5d1683d7bb47a345d8c8d33cfb4a32
Menno Finlay-Smits <inbox@menno.io>
2018-08-29 16:16:46 +1200

    Retries I2C writes
    This helps to deal with occasional failures due to "remote I/O
    error". A single retry appears to resolve the issue in observed cases.

--- attiny.go
+++ attiny.go
@@ -19,29 +19,31 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import (
-	"encoding/binary"
 	"sync"
 	"time"
 
 	"periph.io/x/periph/conn/i2c"
 	"periph.io/x/periph/conn/i2c/i2creg"
-	"periph.io/x/periph/conn/mmr"
 	"periph.io/x/periph/host"
 )
 
 const (
 	attinyAddress = 0x04
 
+	watchdogReg = 0x12
+	sleepReg    = 0x11
+
 	// 3 was just a randomly chosen as the number for the attiny to return
 	// to indicate its presence.
 	magicReturn = 0x03
 
 	// Check for the ATtiny for up to a minute.
-	connectAttempts        = 20
+	maxConnectAttempts     = 20
 	connectAttemptInterval = 3 * time.Second
 
-	watchdogTimerAddress = 0x12
-	sleepAddress         = 0x11
+	// Parameters for transaction retries.
+	maxTxAttempts   = 5
+	txRetryInterval = time.Second
 )
 
 // connectATtiny sets up a i2c device for talking to the ATtiny and
@@ -51,16 +53,11 @@ func connectATtiny() (*attiny, error) {
 	if _, err := host.Init(); err != nil {
 		return nil, err
 	}
-
 	bus, err := i2creg.Open("")
 	if err != nil {
 		return nil, err
 	}
-
-	dev := &mmr.Dev8{
-		Conn:  &i2c.Dev{Bus: bus, Addr: attinyAddress},
-		Order: binary.LittleEndian,
-	}
+	dev := &i2c.Dev{Bus: bus, Addr: attinyAddress}
 
 	if !detectATtiny(dev) {
 		bus.Close()
@@ -69,21 +66,27 @@ func connectATtiny() (*attiny, error) {
 	return &attiny{dev: dev}, nil
 }
 
-func detectATtiny(dev *mmr.Dev8) bool {
-	for i := 0; i < connectAttempts; i++ {
-		time.Sleep(connectAttemptInterval)
-
-		value, err := dev.ReadUint8(0)
-		if err == nil && value == magicReturn {
+func detectATtiny(dev *i2c.Dev) bool {
+	attempts := 0
+	for {
+		b := make([]byte, 1)
+		err := dev.Tx(nil, b)
+		if err == nil && b[0] == magicReturn {
 			return true
 		}
+
+		attempts++
+		if attempts >= maxConnectAttempts {
+			return false
+		}
+
+		time.Sleep(connectAttemptInterval)
 	}
-	return false
 }
 
 type attiny struct {
 	mu  sync.Mutex
-	dev *mmr.Dev8
+	dev *i2c.Dev
 }
 
 // PowerOff asks the ATtiny to turn the system off for the number of
@@ -92,15 +95,39 @@ func (a *attiny) PowerOff(minutes int) error {
 	if minutes <= 0 {
 		return nil
 	}
-	a.mu.Lock()
-	defer a.mu.Unlock()
-	return a.dev.WriteUint16(sleepAddress, uint16(minutes))
+	lb := byte(minutes / 256)
+	rb := byte(minutes % 256)
+	return a.write(sleepReg, []byte{lb, rb})
 }
 
 // PingWatchdog ping's the ATTiny's watchdog timer to prevent it from
 // rebooting the system.
 func (a *attiny) PingWatchdog() error {
+	return a.write(watchdogReg, nil)
+}
+
+func (a *attiny) write(reg uint8, b []byte) error {
+	buf := make([]byte, 1, 1+len(b))
+	buf[0] = byte(reg)
+	buf = append(buf, b...)
+	return a.tx(nil, buf)
+}
+
+func (a *attiny) tx(w, r []byte) error {
 	a.mu.Lock()
 	defer a.mu.Unlock()
-	return a.dev.Conn.Tx([]byte{watchdogTimerAddress}, nil)
+
+	attempts := 0
+	for {
+		err := a.dev.Tx(r, w)
+		if err == nil {
+			return nil
+		}
+
+		attempts++
+		if attempts >= maxTxAttempts {
+			return err
+		}
+		time.Sleep(txRetryInterval)
+	}
 }
